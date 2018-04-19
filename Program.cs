ded4cd07ff35fb3e506150423fb48bfdbcadb253
ASUS\USER <raer1332@gmail.com>
2018-04-19 15:53:38 +0800

    no message

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _412class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // put each number into "list" 
+            List<int> numbers = new List <int>();
+
+            for (int i = 1; i <= 42; i++)
+            {
+                numbers.Add(i);
+            }
+
+            Random rand = new Random();
+            for (int i = 0; i < 6; i++)
+            {
+                // print any number at random
+                int r = rand.Next(1, numbers.Count);// numbers.count-->因為list的總數變少
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write(numbers[r]+" ");
+               
+                // and delete the number printed from "list"
+                numbers.RemoveAt(r);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
